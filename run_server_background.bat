b7e59d54aac0e88ca91c80949df5eb0bdda6e98b
SoDam AI Studio <startmxk@gmail.com>
2026-03-19 22:48:55 +0900

    Delete run_server_background.bat

--- run_server_background.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-:: Use local portable node if available, else system node
-if exist "node\node.exe" (
-    powershell -WindowStyle Hidden -Command "Start-Process 'node\node.exe' -ArgumentList 'server.js' -WorkingDirectory '%~dp0' -WindowStyle Hidden"
-) else (
-    powershell -WindowStyle Hidden -Command "Start-Process 'node' -ArgumentList 'server.js' -WorkingDirectory '%~dp0' -WindowStyle Hidden"
-)
